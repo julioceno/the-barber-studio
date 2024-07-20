11d142c60a89e1011e0320a8a7f0c9767c180edb
Júlio Nepomuceno <julio.nepomuceno@eurekalabs.com.br>
2024-07-20 18:31:14 -0300

    fix: adjusting color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,18 +11,20 @@ const barlow = Barlow({ subsets: ["latin"], weight: ['400', "500", "600", "700",
 
 export default function Home() {
   return (
-    <div className={`bg-[#F8F5EF] h-full max-w-[2000px] mx-auto ${barlow.className}`}>
-      <header>
-        <Main />
-      </header>
-      <main>
-        <InfosBarber />
-        <ServiceQuality />
-        <OurServices />
-        <Course />
-        <Address />
-      </main>
-      <Footer />
+    <div className="bg-black w-full h-full">
+      <div className={`bg-[#F8F5EF] h-full max-w-[2000px] mx-auto ${barlow.className}`}>
+        <header>
+          <Main />
+        </header>
+        <main>
+          <InfosBarber />
+          <ServiceQuality />
+          <OurServices />
+          <Course />
+          <Address />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
