21274d8ff82a481c26273115ed499d90ea9fd36e
Júlio Nepomuceno <julio.nepomuceno@eurekalabs.com.br>
2024-07-20 16:38:53 -0300

    refactor: replace pc to rem

--- src/components/QualityService/index.tsx
+++ src/components/QualityService/index.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { ItemInfo } from "./components/ItemInfo";
 
 
-// TODO: utilizar o componente Image do next
 export function ServiceQuality() {
   const [year, setYear] = useState<string>('');
 
@@ -16,10 +16,16 @@ export function ServiceQuality() {
 
   return (
     <section className="flex flex-col justify-between gap-8 md:gap-1 md:flex-row-reverse mx-5 lg:mx-32 2xl:mx-36 mt-10 md:mt-32">
-        <img 
-          src='/man-cutting-hair-secondary.png'
-          className="w-full h-36 bp-330px:h-48 md:h-[28rem] md:max-w-[400px] 2xl:h-[40rem] 2xl:max-w-[40rem] object-cover rounded-md md:rounded-none"
-        />
+        <div className="relative w-full h-36 bp-330px:h-48 md:h-[28rem] md:max-w-[25rem] 2xl:h-[40rem] 2xl:max-w-[40rem] rounded-md md:rounded-none overflow-hidden">
+          <Image 
+            src='/man-cutting-hair-secondary.png'
+            layout="fill"
+            objectFit="cover"
+            alt="Homem cortando cabelo"
+            quality={90}
+            className="rounded-md md:rounded-none"
+          />
+        </div>
         <div className="flex flex-col text-center lg:text-start gap-3 2xl:self-center md:w-6/12 self-start items-center lg:items-start">
           <h5 className="font-black text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl md:w-8/12 md:tracking-wide lg:tracking-widest" >SERVIÇOS DE QUALIDADE</h5>
           <p className="leading-6  text-sm md:text-md lg:text-lg md:w-9/12 lg:mt-5">
